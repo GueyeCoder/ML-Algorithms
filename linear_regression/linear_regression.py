e8f4e79b93edefea837c43cb00aaf3dc3c1c1819
Assane Gueye <14497532+GueyeCoder@users.noreply.github.com>
2025-06-06 18:48:00 +0000

    initial commit for linear regression algorithm

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -1,4 +1,4 @@
-
+import numpy as np 
 
 class LinearRegression:
     def __init__(self, l_rate=0.001, n_iters=1000):
@@ -8,9 +8,19 @@ class LinearRegression:
         self.bias = None
 
     def fit(self, X, y):
-        pass
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X, self.weights) + self.bias
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1/n_samples) * np.sum(y_predicted - y)
+            self.weights -= self.l_rate * dw
+            self.bias -= self.l_rate * db
+
 
     def predict(self, X):
-        pass
+        y_predicted = np.dot(X, self.weights) + self.bias
+        return y_predicted
 
     
--- linear_regression/main.py
+++ linear_regression/main.py
@@ -6,8 +6,12 @@ from sklearn.model_selection import train_test_split
 X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
+# fit = plt.figure(figsize=(8,6))
+# plt.scatter(X[:,0], y, c='b', marker='o', s=30)
+# plt.show()
 
+from linear_regression import Linear_regression
 
-fit = plt.figure(figsize=(8,6))
-plt.scatter(X[:,0], y, c='b', marker='o', s=30)
-plt.show()
+regressor = Linear_regression()
+regressor.fit(X_train, y_train)
+predicted = regressor.predict(X_test)
